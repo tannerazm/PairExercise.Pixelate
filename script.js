44ae38280d5e71e35e5bb32a012e35970fe3ddd5
Tanner <tannerazm@yahoo.com>
2022-06-21 13:18:31 -0600

    updated docs ready for npm

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your code here
+const ul = document.getElementById('the-ul')
+const newLi = document.createElement('li')
+ul.appendChild(newLi)
+const table = document.getElementsByTagName('table')[0]
+
+function makeRow () {
+    const row = document.createElement('tr')
+    for(let i = 0;i < 20;i++) {
+        const td = document.createElement('td')
+
+        row.appendChild(td)
+}
+table.appendChild(row)
+}
+
+makeRow()
